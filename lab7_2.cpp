9d1b68d217cc8b9eb8fd4c9a0400e65fb2e12cc0
Rada <sirada_dee@cmu.ac.th>
2025-12-12 17:26:16 +0700

    lab7_2.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,3 +1,45 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    string name,movie,day,ans;
+    int id ;
+
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ": ";
+    cin >> id;
+    cin.ignore();
+
+    int gear = (id/pow(10,7))-12;
+
+    cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name << ": ";
+    getline(cin,movie);
+    
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+
+    cout << name << ": ";
+    getline(cin,day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+
+    cout << name << ": ";
+    getline(cin,ans);
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+
+
+}
+
 //Dialog of the first example is given below.
 
 /*
