1811a67c3cbce1ce2e8781a7231dfca9ec829f4b
Rada <sirada_dee@cmu.ac.th>
2025-12-12 17:44:41 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	if(x<'A' || x>'Z')
+		return '0';
+	if (x=='A')
+		return 'Z';
+
+	return x-1;
 }
 
 int main(){
